053a9825ccf6f37dedd5ded56fd8d5a97ab74fb7
Tom Hendra <tomhendra@TomHs-MacBook-Pro.local>
2021-03-26 10:37:21 +0000

    fixed full width issue of heroimage

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   landingImage: {
-    marginRight: "-20px",
+    marginRight: "-40px",
     width: "calc(50% + 20px)",
     [theme.breakpoints.down("md")]: {
       width: "50%",
